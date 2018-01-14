52a8bf02aed7ca70777bb4215742fc3e37465f0c
Christian Illies <post@christianillies.de>
2018-01-14 19:58:32 +0100

    added content template parts inside of THE LOOP

--- single.php
+++ single.php
@@ -1,8 +1,11 @@
 <?php get_header(); ?>
 <main>
-<article>
-    <p>Hauptinhalt single.php</p>
-</article>
-<?php get_sidebar(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php get_template_part('template_parts/content'); ?>
+    <?php endwhile; else : ?>
+    <!-- WP sucht nach "content-error.php" mit automatischer Fallback lösung auf content.php -->
+    <?php get_template_part('template_parts/content', 'error'); ?>
+    <?php endif; ?>
+    <?php get_sidebar(); ?>
 </main>
 <?php get_footer(); ?>
