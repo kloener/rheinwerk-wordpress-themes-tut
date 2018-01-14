b67d08140b1c2ab69eb48e655f7c9bc2fc86d65b
Christian Illies <post@christianillies.de>
2018-01-14 20:13:22 +0100

    comments

--- single.php
+++ single.php
@@ -2,7 +2,8 @@
 <main>
     <div class="articles">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php get_template_part('template_parts/content'); ?>
+        <!-- WP sucht nach "content-single.php" mit automatischer Fallback lösung auf content.php -->
+        <?php get_template_part('template_parts/content', 'single'); ?>
         <?php endwhile; else : ?>
         <!-- WP sucht nach "content-error.php" mit automatischer Fallback lösung auf content.php -->
         <?php get_template_part('template_parts/content', 'error'); ?>
