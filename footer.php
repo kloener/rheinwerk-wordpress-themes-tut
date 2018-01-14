2e107a23b9bcd1e0884511251729c99ecf646cfc
Christian Illies <post@christianillies.de>
2018-01-14 20:11:53 +0100

    content site width articles floating

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
-<footer><p>&copy; Copyright 2017 - <?php date('Y'); ?> &dash; <?php bloginfo('name'); ?></p></footer>
+    <footer><p>&copy; Copyright 2017 - <?php date('Y'); ?> &dash; <?php bloginfo('name'); ?></p></footer>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
