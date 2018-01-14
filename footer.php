be7952b30822a924a5103fb3d398f38bd9f8790d
Christian Illies <post@christianillies.de>
2018-01-14 19:44:12 +0100

    footer function to include admin bar

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer><p>&copy; Copyright 2017 - <?php date('Y'); ?> &dash; <?php bloginfo('name'); ?></p></footer>
-
+<?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <title><?php wp_title(''); ?> <?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
