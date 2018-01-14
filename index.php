891f4b55b6dc91fd13426becf72d066588af23cd
Christian Illies <post@christianillies.de>
2018-01-14 19:24:01 +0100

    static templates added

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head></head>
+<body>
+
+<header>
+<h1><?php bloginfo('name'); ?></h1>
+<p>Slogan</p>
+<nav>Navigation</nav>
+</header>
+<main>
+<article></article>
+<aside></aside>
+</main>
+<footer><p>Copyright</p></footer>
+
+</body>
+</html>
