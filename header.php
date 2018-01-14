21730ac4034466bbd5619db8b8c568f5e54c4b1c
Christian Illies <post@christianillies.de>
2018-01-14 19:36:05 +0100

    added body_classes

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="<?php bloginfo('language'); ?>">
 <head>
-<title><?php wp_title(''); ?> <?php bloginfo('name'); ?></title>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <title><?php wp_title(''); ?> <?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
-<body>
+<body <?php body_class(); ?>>
 
 <header>
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
-<?php get_template_part('template_parts/navi'); ?>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+    <?php get_template_part('template_parts/navi'); ?>
 </header>
