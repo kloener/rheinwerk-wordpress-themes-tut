8baafd92dc9d3fa08fe416bfef24faa4b3f00a3d
Christian Illies <post@christianillies.de>
2018-01-14 20:00:06 +0100

    added perma link for posts

--- template_parts/content.php
+++ template_parts/content.php
@@ -1,4 +1,4 @@
 <article <?php post_class(); ?>>
-    <h2><?php the_title(); ?></h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php the_content(); ?>
 </article>
