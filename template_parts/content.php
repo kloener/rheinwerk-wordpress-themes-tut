f37aceb7fd2b1b3be74610e6021641fc90e14bf2
Christian Illies <post@christianillies.de>
2018-01-14 20:03:51 +0100

    author and date added

--- template_parts/content.php
+++ template_parts/content.php
@@ -1,4 +1,5 @@
 <article <?php post_class(); ?>>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p>Von <?php the_author(); ?> am <?php the_time('d.m.Y'); ?> veröffentlicht.</p>
     <?php the_content(); ?>
 </article>
