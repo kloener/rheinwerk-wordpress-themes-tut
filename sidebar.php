0bc45a7dfed2c5455fa12fe48530b270c47f29dc
Christian Illies <post@christianillies.de>
2018-01-15 20:06:08 +0100

    3.9 - conditional tags

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,18 @@
+<?php if(is_page() && !is_page('Kontakt')): ?>
 <aside>
-    Sidebar
+    <h4>Haben Sie Fragen?</h4>
+    <p>Nehmen Sie gerne <a href="<?php ?>">Kontakt</a> mit uns auf.</p>
 </aside>
+<?php endif; ?>
+<?php if(is_single()): ?>
+<aside>
+    <ul>
+        <?php wp_list_categories(); ?>
+    </ul>
+</aside>
+<?php endif; ?>
+<?php if(is_front_page()): ?>
+<aside>
+    <h4>Willkommen!</h4>
+</aside>
+<?php endif; ?>
